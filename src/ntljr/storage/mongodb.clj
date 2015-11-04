764d5f7d6a2c0584e6433068360c2f210e076649
Yuri Zhykin <yuri.zhykin@gmail.com>
2015-11-05 01:13:24 +0200

    experiments with mongodb; also started a CHANGELOG :)

--- src/ntljr/storage/mongodb.clj
+++ src/ntljr/storage/mongodb.clj
@@ -1,10 +1,14 @@
 (ns ntljr.storage.mongodb
   (:require [monger.core :as mg]))
 
-(defn connect [host port]
+(defn connect
+  "Get a connection object for a given db server."
+  [host port]
   (let [server-addr (mg/server-address host port)]
     (mg/connect server-addr)))
 
+(defn- get-db [conn dbname]
+  (mg/get-db dbname))
 
 (defn add-definition
   "Add definition to the persistent storage."
@@ -17,6 +21,3 @@
   [conn query]
   ;; TODO: impl
   nil)
-
-
-
